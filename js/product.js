4d14eabb7f2f1711355d92e4285462e12630dc89
Cecilie <cepe0001@stud.kea.dk>
2024-02-05 14:25:13 +0100

    tilføjet js til produkt

--- js/product.js
+++ js/product.js
@@ -13,3 +13,14 @@ function sidenVises() {
 }
 
 // https://kea-alt-del.dk/t7/api/products/1163
+
+fetch("https://kea-alt-del.dk/t7/api/products/1533")
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
+
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".purchaseBox h3").textContent = product.productdisplayname;
+  document.querySelector(".purchaseBox .brand").textContent = product.brandname;
+  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+}
